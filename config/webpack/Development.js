5f0d1d2692d6605e9ac14c2331ec8858d1c74bf8
hiro <hiro.kikuchi.universe@gmail.com>
2019-05-30 14:59:43 +0700

    + add webpack dev server setting

--- config/webpack/Development.js
+++ config/webpack/Development.js
@@ -10,6 +10,10 @@ class WebpackDevConfig extends WebpackBaseConfig {
     this.config = {
       // Set extra webpack configuration like;
       devtool: '',
+      devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        port: 3000,
+      },
       entry: '',
       plugins: [
         new webpack.HotModuleReplacementPlugin(),
