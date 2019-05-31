337852b70cdaf6b31f9d25f6d34d60cd5a9d63d0
HIRONORI KIKUCHI <hiro.kikuchi.universe@gmail.com>
2019-05-31 23:52:57 +0700

    * update webpac config to basic one

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,51 @@
-'use strict';
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-const webpackConfigs = require('./config/webpack');
-const defaultConfig = 'dev';
+const htmlWebpackPlugin = new HtmlWebpackPlugin({
+  template: './public/index.html',
+  filename: 'index.html',
+});
 
-module.exports = (configName) => {
-
-  const requestedConfig = configName || defaultConfig;
-
-  let LoadedConfig = defaultConfig;
-
-  if (webpackConfigs[requestedConfig] !== undefined) {
-    LoadedConfig = webpackConfigs[requestedConfig];
-  } else {
-    LoadedConfig = webpackConfigs[defaultConfig];
-  }
-
-  const loadedInstance = new LoadedConfig();
-
-  process.env.NODE_ENV = loadedInstance.env;
-
-  return loadedInstance.config;
+module.exports = {
+  entry: path.resolve('./src'),
+  output: {
+    path: path.resolve('./dist'),
+    filename: 'bundle.js',
+  },
+  devServer: {
+    contentBase: path.join('./dist'),
+    compress: true,
+    port: 3000,
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+    alias: {
+      '@': path.resolve('./src'),
+      // TODO: config
+      // 'config': `${path.resolve('./src')}/config/`,
+    },
+    modules: [
+      'node_modules',
+      'bower_components',
+    ],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ]
+  },
+  plugins: [
+    htmlWebpackPlugin,
+    new webpack.HotModuleReplacementPlugin(),
+  ],
 };
